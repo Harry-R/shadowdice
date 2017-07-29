57ec0254757c9b4d9eddd6885e36953d098dce20
Leonard Rapp <leonardrapp@gmx.de>
2017-07-29 22:45:50 +0200

    added android studio project

---
+++ app/src/main/java/de/harryr/shadowdice/MainActivity.java
@@ -0,0 +1,13 @@
+package de.harryr.shadowdice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
