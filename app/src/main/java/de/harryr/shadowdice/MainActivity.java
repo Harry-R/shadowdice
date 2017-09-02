7a0ebfef7b4c1b70f7e002ccbbc72ecd8f8872fa
Leonard Rapp <leonardrapp@gmx.de>
2017-09-02 19:03:32 +0200

    UI: fix numpicker bug

--- app/src/main/java/de/harryr/shadowdice/MainActivity.java
+++ app/src/main/java/de/harryr/shadowdice/MainActivity.java
@@ -2,12 +2,23 @@ package de.harryr.shadowdice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.NumberPicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    NumberPicker numpicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // find views
+        numpicker = (NumberPicker) findViewById(R.id.numpicker);
+
+        // set up numpicker
+        numpicker.setMaxValue(30);
+        numpicker.setMinValue(0);
+        numpicker.setWrapSelectorWheel(false);
     }
 }
